4face062b550439b1dca96a9d50b0048a99c89ff
aCordovaPCD <e-adrian_cordova@usiglobal.com>
2025-05-09 14:41:07 -0600

    deploy GHPages

---
+++ EjercicioAnalytics360API/Models/Balanceo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjercicioAnalytics360API.Models;
+
+public partial class Balanceo
+{
+    public int Id { get; set; }
+
+    public string? Input { get; set; }
+
+    public sbyte? Result { get; set; }
+
+    
+}
